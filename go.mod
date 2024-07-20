d903c069c831c8ac439c89a6ce108ffc7311e59b
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2024-07-20 11:17:50 +0100

    feat: prelims

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/glamboyosa/unkeygoratelimit
+
+go 1.22.5
---
+++ types/types.go
@@ -0,0 +1,20 @@
+package types
+
+type UnkeyRateLimiterPayload struct {
+	Namespace  string          `json:"namespace"`
+	Identifier string          `json:"identifier"`
+	Limit      int             `json:"limit"`
+	Duration   int             `json:"duration"`
+	Cost       int             `json:"cost"`
+	Async      bool            `json:"async"`
+	Meta       UnkeyMeta       `json:"meta"`
+	Resources  []UnkeyResource `json:"resources"`
+}
+
+type UnkeyResource struct {
+	Type string `json:"type"`
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type UnkeyMeta map[string]interface{}
